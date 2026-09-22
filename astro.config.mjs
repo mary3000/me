a0eaa241cba1590c4bfb4b15ee14f2186ed1b888
Mariia Feofanova <mary.fifa3000@gmail.com>
2026-09-22 23:32:18 +0100

    Fix GitHub Pages asset paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,8 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-export default defineConfig({site:"https://mfeofanova.dev",integrations:[tailwind()]});
+
+export default defineConfig({
+  site: "https://mary3000.github.io",
+  base: "/mfeofanova-website",
+  integrations: [tailwind()],
+});
